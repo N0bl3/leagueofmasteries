5b6608326e1c99855918f731ff7f26df0ec1447a
Alexandre Noble <carbonealpha@gmail.com>
2016-04-26 20:33:03 +0000

    First interactive simple homepage

---
+++ public/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("form#summ-by-name").submit(function(event){
+   event.preventDefault();
+   var submission = $(this).serializeArray();
+
+   var summonerName = submission[0].value;
+   var platformId = submission[1].value;
+
+//   console.log(platformId + " " + summonerName);
+   window.location.href = window.location.href + platformId + "/" + summonerName;
+
+  });
+});
