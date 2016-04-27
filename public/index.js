a1ed55b15c9c92cfd449d7066609aa1c48eee1fd
Alexandre Noble <carbonealpha@gmail.com>
2016-04-27 17:18:28 +0000

    ADD get player ID ADD get player region ADD get player progression for each champion ADD leaderboard feature

--- public/index.js
+++ public/index.js
@@ -1,13 +1,47 @@
 $(document).ready(function(){
+	var playerId,
+	playerName,
+	region,
+	playerScore;
+	
   $("form#summ-by-name").submit(function(event){
    event.preventDefault();
    var submission = $(this).serializeArray();
 
-   var summonerName = submission[0].value;
-   var platformId = submission[1].value;
+   playerName = submission[0].value;
+   region = submission[1].value;
+   $.get(window.location.href + region + "/sname/" + playerName)
+   .done(function(data){
+   	console.log(data);
+   	$("#response-zone").text(data);
+   	data = JSON.parse(data);
+   	playerId = data.id;
+   })
+   .then(function(){
+   	$.get(window.location.href + region + "/pid/" + playerId)
+   	.done(function(data){
+   		playerScore = data;
+   	})
+   	.then(function(){
+   		$("#player-score").text(playerScore);
+   	});
+   });
 
-//   console.log(platformId + " " + summonerName);
-   window.location.href = window.location.href + platformId + "/sname/" + summonerName;
-
+  });
+  
+  $("button#leaderboards-button").click(function(){
+  	$.get("")
+  	.done(function(data){
+  		
+  	});
+  });
+  
+  $("button#progression-button").click(function(){
+  	console.log(region, playerId);
+  	$.get(window.location.href + region + "/pid/" + playerId + "/champions")
+  	.done(function(data){
+   	console.log(data);
+   	$("#progression-zone").text(data);
+  	});
   });
 });
