5c4681b4aac04a85a4239d357e00a24065322a96
Alexandre Noble <carbonealpha@gmail.com>
2016-04-28 01:47:27 +0000

    HAndle routes Get images from Riot servers Display a progress bar Handle errors on server

--- public/index.js
+++ public/index.js
@@ -1,59 +1,90 @@
-$(document).ready(function(){
-	var playerId,
-	playerName,
-	region,
-	playerScore;
-	
-  $("form#summ-by-name").submit(function(event){
-   event.preventDefault();
-   var submission = $(this).serializeArray();
+/*jshint browser:true, jquery:true*/
+$(document).ready(function () {
+    var playerId,
+        playerName,
+        region,
+        playerScore;
 
-   playerName = submission[0].value;
-   region = submission[1].value;
-   $.getJSON(window.location.href + region + "/sname/" + playerName)
-   .done(function(json){
-   	$("#response-zone").text(JSON.stringify(json));
-   	playerId = json.id;
-   })
-   .then(function(){
-   	$.getJSON(window.location.href + region + "/pid/" + playerId)
-   	.done(function(score){
-   		playerScore = score;
-   	})
-   	.then(function(){
-   		$("#player-score").text(playerScore);
-   	});
-   	$("button").removeAttr("disabled");
-   });
+    $("form#summ-by-name").submit(function (event) {
+        event.preventDefault();
+        var submission = $(this).serializeArray();
 
-  });
-  
-  $("button#leaderboards-button").click(function(){
-  	$.getJSON("")
-  	.done(function(data){
-  		
-  	});
-  });
-  
-  $("button#progression-button").click(function(){
-  	$.getJSON(window.location.href + region + "/pid/" + playerId + "/champions")
-  	.done(function(arr){
-  	var text = "";
-   	arr.forEach(function(champion){
-   		text += text ? "<br>ID : " + champion.championId + " Grade : " + (champion.highestGrade||"None") : "ID : " + champion.championId + " Grade : " + (champion.highestGrade||"None");
-   	});
-   	$("#progression-zone").append(text);
-  	});
-  });
-  
-  $("button#get-top-ten").click(function(){
-  	$.getJSON(window.location.href + region + "/pid/" + playerId + "/top" + "?n=10")
-  	.done(function(arr){
-  		var text = "";
-		arr.forEach(function(champion){
-			text += text ? "<br>ID : " + champion.championId + " Grade : " + (champion.highestGrade||"None") : "ID : " + champion.championId + " Grade : " + (champion.highestGrade||"None");
-		});
-  		$("#top-zone").append(text);
-  	});
-  });
+        playerName = submission[0].value;
+        region = submission[1].value;
+        $.getJSON(window.location.href + region + "/sname/" + playerName)
+            .done(function (json) {
+                $("#response-zone").text("");
+                $("#top-zone").text("");
+                $("#progression-zone").text("");
+                $("#response-zone").append("<p>profileIconId : " + json.profileIconId + "</p><h3>ID: " + json.id + " - " + json.name + "</h3><h4>Level: " + json.summonerLevel + "</h4>");
+                playerId = json.id;
+            })
+            .then(function () {
+                $.getJSON(window.location.href + region + "/pid/" + playerId)
+                    .done(function (score) {
+                        $("#player-score").text("");
+                        playerScore = score;
+                    })
+                    .then(function () {
+                        var percentage = playerScore * 100 / (130 * 5);
+                        var progressClass;
+                        if (percentage <= 25) {
+                            progressClass = "progress-bar-danger";
+                        } else if (percentage > 25 && percentage <= 50) {
+                            progressClass = "progress-bar-warning";
+                        } else if (percentage > 50 && percentage <= 75) {
+                            progressClass = "progress-bar-info";
+                        } else {
+                            progressClass = "progress-bar-success";
+                        }
+                        $("#player-score").append("<h4>Total score : " + playerScore + "</h4>")
+                            .append('<div class="progress">' +
+                                '<div class="progress-bar ' + progressClass + '" role="progressbar" aria-valuenow="' + percentage + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + percentage + '%;">' +
+                                '<span class="sr-only">' + percentage + '% Complete</span>' +
+                                '</div></div>')
+                            .append("<div class='col-xs-12'><p>" +
+                                "You have " + playerScore + "/650 to get all champs at max level" +
+                                "</p></div>");
+                    });
+                $("button").removeAttr("disabled");
+            });
+
+    });
+
+    $("button#leaderboards-button").click(function () {
+        $.getJSON("")
+            .done(function () {});
+    });
+
+    $("button#progression-button").click(function () {
+        $.getJSON(window.location.href + region + "/pid/" + playerId + "/champions")
+            .done(function (arr) {
+                $("#top-zone").text("");
+                $("#progression-zone").text("");
+                arr.forEach(function (champion) {
+                    $("#progression-zone").append("<div class='col-xs-3'><p>" +
+                        "<img src='http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/" + champion.image.full + "' alt='" + champion.name + "'><br><span id='champion-name'>" +
+                        champion.name +
+                        "</span><br>Grade:Level<br><span id='champion-grade'>" +
+                        (champion.highestGrade || "None") + " : " + (champion.championLevel || 0) +
+                        "</span></p></div>");
+                });
+            });
+    });
+
+    $("button#get-top-ten").click(function () {
+        $.getJSON(window.location.href + region + "/pid/" + playerId + "/top?count=12")
+            .done(function (arr) {
+                $("#progression-zone").text("");
+                $("#top-zone").text("");
+                arr.forEach(function (champion) {
+                    $("#top-zone").append("<div class='col-xs-3'><p>" +
+                        "<img src='http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champion.key + "_0.jpg' alt='" + champion.name + "' class='top-img'><br><span id='champion-name'>" +
+                        champion.name +
+                        "</span><br>Grade:<br><span id='champion-grade'>" +
+                        (champion.highestGrade || "None") +
+                        "</span></p></div>");
+                });
+            });
+    });
 });
--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 
 var champions;
 
-function regionToPlatformId(region){
-	switch (region){
+function regionToPlatformId(region) {
+	switch (region) {
 		case "eune":
 			return "eun1";
 		case "euw":
@@ -48,59 +48,65 @@ function regionToPlatformId(region){
 	}
 }
 
-function champIdToChampObject(champId){
-		champions.forEach(function(champion){
-			if (champId == champion.id) {
-				return champion;
-			}
-		});
-		return false;
+function champIdToChampObject(champId) {
+	for (var champion in champions) {
+		if (champId == champions[champion].id) {
+			return champions[champion];
+		}
+	}
+	return false;
 }
 
 //Get champions list
 request({
-		url: "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?" + api,
-		method: "GET",
-		json: true
-}, function(error, response, body){
-		if (!error && response.statusCode == 200){
-			champions = body.data;
-		} else {
-			console.error("Error retrieving champions\nStatus Code : " + response.statusCode);
+	url: "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image&" + api,
+	method: "GET",
+	json: true
+}, function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+		champions = body.data;
+		for (var champion in champions) {
+			console.log(champion);
 		}
+	} else {
+		console.error("Error retrieving champions\nStatus Code : " + response.statusCode);
+	}
 });
 
 app.get("/", function (req, res) {
-		res.render('index.pug', {champions: champions}, function (err, html) {
-    		if(!err){
-        		res.send(html);
-        	} else {
-        		console.error(err);
-        		res.end("Error while starting app!");
-        	}
-    	});
+	res.render('index.pug', {
+		champions: champions
+	}, function (err, html) {
+		if (!err) {
+			res.send(html);
+		} else {
+			console.error(err);
+			res.end("Error while starting app!");
+		}
+	});
 });
 
 //Get summoner info from his name and location
 app.get("/:region/sname/:summonerName", function (req, res) {
 	console.log(req.params);
 	var region = req.params.region;
-	var summonerName =req.params.summonerName;
-	if (/br|eune|euw|kr|lan|las|oce|ru|tr|na|jp/.test(req.params.region) && /^[a-zA-Z0-9]+$/.test(req.params.summonerName)){
-    request({
-        url: "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?" + api,
-        method: "GET",
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body[summonerName]);
-        } else {
-            console.error("Error at endpoint : /:region/:summonerName\nStatus Code : " + response.statusCode);
-        }
-    });
-    } else {
-    	res.sendStatus(400).end();
-    }
+	var summonerName = req.params.summonerName;
+	if (/br|eune|euw|kr|lan|las|oce|ru|tr|na|jp/.test(req.params.region) && /^[a-zA-Z0-9]+$/.test(req.params.summonerName)) {
+		request({
+			url: "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?" + api,
+			method: "GET",
+			json: true
+		}, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				res.send(body[summonerName]);
+			} else {
+				console.error("Error at endpoint : /:region/:summonerName\nStatus Code : " + response.statusCode);
+				res.sendStatus(response.statusCode);
+			}
+		});
+	} else {
+		res.sendStatus(400);
+	}
 });
 
 //Get a champion mastery by player id and champion id. Response code 204 means there were no masteries found for given player id or player id and champion id combination. (RPC)
@@ -110,19 +116,20 @@ app.get("/:region/pid/:playerId/champid/:championId", function (req, res) {
 	var platformId = regionToPlatformId(req.params.region);
 	var playerId = req.params.playerId;
 	var championId = req.params.championId;
-    request({
-        url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/champion/" + championId + "?" + api,
-        method: "GET",
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        } else if(!error && response.statusCode == 204) {
-        	res.status(204).end();
-        } else {
-            console.error("Error at endpoint : /:platformId/pid/:playerId/:championId\nStatus Code : " + response.statusCode);
-        }
-    });
+	request({
+		url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/champion/" + championId + "?" + api,
+		method: "GET",
+		json: true
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		} else if (!error && response.statusCode == 204) {
+			res.status(204).end();
+		} else {
+			console.error("Error at endpoint : /:platformId/pid/:playerId/:championId\nStatus Code : " + response.statusCode);
+			res.sendStatus(response.statusCode);
+		}
+	});
 });
 
 //Get all champion mastery entries sorted by number of champion points descending (RPC)
@@ -131,17 +138,22 @@ app.get("/:region/pid/:playerId/champions", function (req, res) {
 	var region = req.params.region;
 	var platformId = regionToPlatformId(req.params.region);
 	var playerId = req.params.playerId;
-    request({
-        url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/champions" + "?" + api,
-        method: "GET",
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        } else {
-            console.error("Error at endpoint : /:platformId/pid/:playerId/champions\nStatus Code : " + response.statusCode);
-        }
-    });
+	request({
+		url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/champions" + "?" + api,
+		method: "GET",
+		json: true
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body.forEach(function (champion) {
+				champion = Object.assign(champion, champIdToChampObject(champion.championId));
+				//				champion.image = getChampImage(champion.championId);
+			});
+			res.send(body);
+		} else {
+			console.error("Error at endpoint : /:platformId/pid/:playerId/champions\nStatus Code : " + response.statusCode);
+			res.sendStatus(response.statusCode);
+		}
+	});
 });
 
 //Get a player's total champion mastery score, which is sum of individual champion mastery levels (RPC)
@@ -151,43 +163,47 @@ app.get("/:region/pid/:playerId", function (req, res) {
 	var platformId = regionToPlatformId(region);
 	var playerId = req.params.playerId;
 	console.log(region, platformId, playerId);
-    request({
-        url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/score" + "?" + api,
-        method: "GET"
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        } else if (!error) {
-        	res.status(response.statusCode).end();
-        } else {
-            console.error("Error at endpoint : /:platformId/pid/:playerId\nStatus Code : " + response.statusCode + " " + body);
-        }
-    });
+	request({
+		url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/score" + "?" + api,
+		method: "GET"
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		} else if (!error) {
+			res.status(response.statusCode).end();
+		} else {
+			console.error("Error at endpoint : /:platformId/pid/:playerId\nStatus Code : " + response.statusCode + " " + body);
+			res.sendStatus(response.statusCode);
+		}
+	});
 });
 
 //Get specified number of top champion mastery entries sorted by number of champion points descending (RPC)
 app.get("/:region/pid/:playerId/top", function (req, res) {
-	if (req.query.n){
-		req.query.n = 3;
-	}
+	var count = req.query.count || 5;
 	console.log(req.params);
 	var region = req.params.region;
 	var platformId = regionToPlatformId(req.params.region);
 	var playerId = req.params.playerId;
-    request({
-        url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/topchampions" + "?count=" + req.query.n + "&" + api,
-        method: "GET",
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        } else {
-            console.error("Error at endpoint : /:platformId/pid/:playerId/top\nStatus Code : " + response.statusCode);
-        }
-    });
+	request({
+		url: "https://" + region + ".api.pvp.net/championmastery/location/" + platformId + "/player/" + playerId + "/topchampions" + "?count=" + count + "&" + api,
+		method: "GET",
+		json: true
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body.forEach(function (champion) {
+				champion = Object.assign(champion, champIdToChampObject(champion.championId));
+				//				champion.image = getChampImage(champion.championId);
+			});
+			res.send(body);
+		} else {
+			console.error("Error at endpoint : /:platformId/pid/:playerId/top\nStatus Code : " + response.statusCode);
+			res.sendStatus(response.statusCode);
+		}
+	});
 });
 
 // Runs the server
 app.listen(appEnv.port, function () {
-    console.log("server starting on " + appEnv.url);
+	console.log("server starting on " + appEnv.url);
 });
